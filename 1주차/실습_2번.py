6e530e8cc224c7e4e59fe3a9b40ca61214e74c89
KYUSEONGHAN <tyty587587@gmail.com>
2022-03-11 11:41:03 +0900

    2022.3.11.금: 1주차 실습 1번 풀이, python3

--- 1주차/실습_2번.py
+++ 1주차/실습_2번.py
@@ -1,6 +1,8 @@
 # EM-01-Python-Ex2: R, L, G, C from Z0, γ and f
-# Input: Z0 = 79 + 3.8e-3j, gamma=6.2e-4+3.4j, f=100e6
+# Input: Z0 = 79+3.8e-3j, gamma=6.2e-4+3.4j, f=100e6
 # Output:
+import cmath
+
 pi = 3.14159265
 
 while True:
@@ -19,3 +21,15 @@ while True:
  print('L(H/m)=', L)
  print('G(S/m)=', G)
  print('C(F/m)=', C)
+
+
+""" output
+Z0(ohm)=79+3.8e-3j
+gamma(per meter)=6.2e-4+3.4j
+f(Hz)=100e6
+R(ohm/m)= 0.03606
+L(H/m)= 4.27490181383e-07
+G(S/m)= 9.918282303601987e-06
+C(F/m)= 6.849706343446908e-11
+Z0(ohm)=
+"""
