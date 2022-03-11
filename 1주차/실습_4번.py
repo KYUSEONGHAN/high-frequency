194b32b8a2e6db4d4d978c45817d0182e274268e
KYUSEONGHAN <tyty587587@gmail.com>
2022-03-11 11:53:19 +0900

    2022.3.11.금: 1주차 실습 4번 풀이, python3

--- 1주차/실습_4번.py
+++ 1주차/실습_4번.py
@@ -5,11 +5,21 @@ while True:
  R = float(input('R(ohm)='))
  L = float(input('L(uH)='))
  C = float(input('C(uF)='))
-
  f = float(input('f(Hz)='))
+
  w = 2 * pi * f
- Zs = complex(R, w*L*1e-6-1 / (w*C*1e-6))
- Zp = 1/complex(1/R, w*C*1e-6-1 / (w*L*1e-6))
+ Zs = complex(R, w*L*1e-6-1 / (w*C*1e-6))      # 직렬회로 임피던스
+ Zp = 1/complex(1/R, w*C*1e-6-1 / (w*L*1e-6))  # 병렬회로 임피던스
 
  print('Z(series)=', Zs)
  print('Z(parallel)=', Zp)
+
+ """ output
+ R(ohm)=100
+ L(uH)=20
+ C(uF)=50
+ f(Hz)=1e6
+ Z(series)= (100+125.66053690148915j)
+ Z(parallel)= (1.0132629437904996e-07-0.0031831791384774404j)
+ R(ohm)=
+ """
