d865c6c9630347126400b2c80e95354dba7bd1aa
KYUSEONGHAN <tyty587587@gmail.com>
2022-03-11 11:50:29 +0900

    2022.3.11.금: 1주차 실습 3번 풀이, python3

--- 1주차/실습_3번.py
+++ 1주차/실습_3번.py
@@ -4,6 +4,7 @@ from cmath import *  # Use the complex math library in Python
 while True:
  z1 = complex(input('z1='))
  z2 = complex(input('z2='))
+
  z3 = z1 * z2
  z4 = z1 / z2
  zp3 = polar(z3)
@@ -13,3 +14,11 @@ while True:
 
  print('z3=', z3, 'z3(polar)=', zp3, 'arg(z3)(deg)=', z3arg)
  print('z4=', z4, 'z4(polar)=', zp4, 'arg(z4)(deg)=', z4arg)
+
+ """ output
+ z1=1+2j
+ z2=3+4j
+ z3= (-5+10j) z3(polar)= (11.180339887498949, 2.0344439357957027) arg(z3)(deg)= 116.56514963544781
+ z4= (0.44+0.08j) z4(polar)= (0.4472135954999579, 0.17985349979247828) arg(z4)(deg)= 10.304855172904833
+ z1=
+ """
